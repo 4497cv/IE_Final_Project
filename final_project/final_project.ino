7c828f09293deefcb28b87b0a981e316e23a035b
4497cv <4497cv@gmail.com>
2019-12-04 21:57:58 -0600

    implementing isr

--- final_project/final_project.ino
+++ final_project/final_project.ino
@@ -41,38 +41,18 @@ static void LM35DZ_get_samples(void);
 static void LM35DZ_print_samples(void);
 static void LM35DZ_calculate_standard_deviation(void);
 
-DHT dht(GPIO_PB1, DHT11);
-
-//enum fsm_states
-//{
-//  GET_SAMPLES,
-//  CALC_VARIANCE,
-//  PRINT_VARIANCE,
-//};
-
-//struct fsm_sys_t
-//{
-//  void(*fptr)();
-//  fsm_states next[NSTATES];
-//};
-
-//static fsm_states current_st = CALC_VARIANCE;
-
-//static fsm_sys_t FSM_SYSTEM[NSTATES]=
-//{
-//  {LM35DZ_print_temperature_celsius, {CALC_VARIANCE, PRINT_RESULT,   GET_SAMPLES}},
-//  {LM35DZ_calc_variance,             {PRINT_RESULT,   GET_SAMPLES, CALC_VARIANCE}},
-//  {LM35DZ_print_temperature_celsius, {GET_SAMPLES,  CALC_VARIANCE,  PRINT_RESULT}}
-//};
+static DHT dht(GPIO_PB1, DHT11);
 
 /* ~~~~~~~~~~ system setup (config) ~~~~~~~~~~  */
 void setup()
 {
+  /* configure adc pin for the temperature sensor */
   pinMode(LM35DZ_ADC_PIN, INPUT);
-  
-  samples_counter = 0;
-  
+  /* start serial communication (uart) @ 115200 baud/s */
   Serial.begin(SERIAL_BAUDRATE);
+
+    
+  samples_counter = 0;
 }
 
 /* ~~~~~~~~~~ infinite loop ~~~~~~~~~~  */
