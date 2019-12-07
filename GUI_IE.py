5752ffdb42abd755c9135470078636973f7cdd35
4497cv <4497cv@gmail.com>
2019-12-07 12:04:19 -0700

    saving changes

--- GUI_IE.py
+++ GUI_IE.py
@@ -12,24 +12,58 @@ try:
 except:
     print("could not import serial module")
 
-
 #GUI Functions
-def serial_port():
-    try:
-        port = Serial('/dev/ttyUSB0', 115200, timeout=0)
-    except:
-        print("tty USB0 not available")
+def serial_port_init():
+    serial_port_id = '/dev/ttyUSB0'
+    global port 
 
+    try:
+        port = Serial(serial_port_id, 115200, timeout=0)
+        print("conncted to " + serial_port_id + "\n\r")
+        app = THDS();
+    except:
+        print("serial port "+ serial_port_id + " is not available\n\r")
+        print("closing program..\n\r")
+
+def serial_port_readline(): 
     while 1:
         port.flush()
         data = port.readline()
         print(data)
 
 def main():
-    try:
-        port = Serial('/dev/ttyUSB0', 115200, timeout=0)
-    except:
-        print("tty USB0 not available")
+    serial_port_init()
+
+def serial_temperature_request():
+    print("requesting temperature to the atmega..\n\r")
+    
+    data = 'REQ_'
+
+    #Waiting for request confirmation
+    while data != 'REQ_TEMP_OK':
+        port.flush()
+        time.sleep(1)
+        port.write('1')
+        time.sleep(1)
+        port.flush()
+        data = port.readline()
+        print(data)
+
+    print("Temperature reading request OK\n\r")
+
+
+
+##EVENT HANDLERS       
+def temperature_button_event_handler():
+    print("temperature button pressed\n\r")
+    serial_temperature_request()
+    serial_port_readline()
+
+def humidity_button_event_handler():
+    print("humidity button pressed\n\r")
+
+def gas_button_event_handler():
+    print("gas button pressed\n\r")
 
 class THDS(tk.Tk):
     def __init__(self, *args, **kwargs):
@@ -55,21 +89,30 @@ class THDS(tk.Tk):
         lbl_empty = tk.Label(self, text = "		", bg="white", width = 17, height=2)
         lbl_empty.grid(column = 3, row = 2)
     
-        btn1 = tk.Button(self, text = "Temperature Sensor", width = 17, height=1, font="Times",borderwidth=3)
+        btn1 = tk.Button(self, text = "Temperature Sensor", width = 17, height=1, font="Times",borderwidth=3, command=temperature_button_event_handler)
         btn1.grid(column = 3, row = 5)
         lbl_empty = tk.Label(self, text = "		", bg="white")
         lbl_empty.grid(column = 3, row = 6)
-        btn2 = tk.Button(self, text = "Humidity Sensor", width = 17, height=1, font="Times",borderwidth=3)
+        btn2 = tk.Button(self, text = "Humidity Sensor", width = 17, height=1, font="Times",borderwidth=3, command=humidity_button_event_handler)
         btn2.grid(column = 3, row = 7)
         lbl_empty = tk.Label(self, text = "		", bg="white")
         lbl_empty.grid(column = 3, row = 8)
-        btn2 = tk.Button(self, text = "Gas Sensor", width = 17, height=1, font="Times",borderwidth=3)
+        btn2 = tk.Button(self, text = "Gas Sensor", width = 17, height=1, font="Times",borderwidth=3, command=gas_button_event_handler)
         btn2.grid(column = 3, row = 9)
         self.mainloop()
         
     def _quit(self):
         self.quit()
         self.destroy()        
+
+class StartPage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+     
+
+#main()
+#GUI_main_menu()
+
 
     # menu = Menu(root)
     # root.config(menu=menu)
@@ -91,14 +134,4 @@ class THDS(tk.Tk):
     # mylist.pack(side=LEFT, fill=BOTH)
     # scrollbar.config(command =mylist.yview)
     # mainloop() # open window   
-
-class StartPage(tk.Frame):
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-     
-
-#main()
-#GUI_main_menu()
-
-app = THDS();
-app.GUI_Quit();
+main()
